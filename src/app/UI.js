9dde36d0d54191c491b807f8f9483c3077e8a6ac
matiasgumma@gmail.com <matiasgumma@gmail.com>
2019-07-11 19:56:36 -0300

    icon + badge added..

--- src/app/UI.js
+++ src/app/UI.js
@@ -4,9 +4,14 @@ export class UI {
         this.temperature = document.getElementById('weather-temperature');
         this.wind = document.getElementById('weather-wind');
         this.statusLs = document.getElementById('statusLs');
+        this.icon = document.getElementById('icon');
+        this.desc = document.getElementById('desc');
     }
 
     display(weather) {
+        this.icon.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+        this.icon.alt = weather.weather[0].main;
+        this.desc.textContent = weather.weather[0].main;
         this.location.textContent = weather.name +' ';
         this.temperature.textContent = weather.main.temp + '°C ';
         this.wind.textContent = weather.wind.speed + 'm/s';
